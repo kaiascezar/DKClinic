ee065faab5b956f269fc05bcf41289eb780b3b5c
dlehd333 <dlehd333@gmail.com>
2020-11-23 18:25:10 +0900

    초기 커밋.

---
+++ DKClinic.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DKClinic.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DKClinic.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("de04a0f2-cf13-4ef2-a299-256ca668d262")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DKClinic.WinformUI/WinformUtility.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DKClinic.MainScene
+{
+    public static class WinformUtility
+    {
+        public static bool AskSure(string question)
+        {
+            return MessageBox.Show(question, "Question",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Question,
+                MessageBoxDefaultButton.Button2) == DialogResult.Yes;
+        }
+
+        public static bool AskDelete()
+        {
+            return false;
+        }
+    }
+}
