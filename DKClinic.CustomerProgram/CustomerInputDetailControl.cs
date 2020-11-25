b3a8264e4df9beaa62d58090fa2bca7aa3a706c3
gun-Daegeungim <daegeungim7301@gmail.com>
2020-11-25 16:44:15 +0900

    event 생성기 통해 생성된 코드 매개변수 타입명 변경 ctmDepChoice -> CustomerDepartmentChoiceControl

--- DKClinic.CustomerProgram/CustomerInputDetailControl.cs
+++ DKClinic.CustomerProgram/CustomerInputDetailControl.cs
@@ -37,14 +37,14 @@ namespace DKClinic.CustomerProgram
         {
             InitializeComponent();
             
-            refCustomerClass = new Data.Customer(); // create customer obj
-            int customerID = refCustomerClass.CustomerID;
+            customerClass = new Data.Customer(); // create customer obj
+            int customerID = customerClass.CustomerID;
             MessageBox.Show($"customID: {customerID}");
 
 
             // 초기 화면출력
-            txbName.Text = refCustomerClass.Name; // 이름 띄우기
-            txbBirthdate.Text = refCustomerClass.Birthdate; // 생년월일 띄우기
+            txbName.Text = customerClass.Name; // 이름 띄우기
+            txbBirthdate.Text = customerClass.Birthdate; // 생년월일 띄우기
 
             customerID = 0;
 
@@ -54,16 +54,16 @@ namespace DKClinic.CustomerProgram
                 ModifyLock(true);
 
                 //기존 데이터 load
-                txbName.Text = refCustomerClass.Name;
-                txbBirthdate.Text = refCustomerClass.Birthdate;
-                txbCellphone.Text = refCustomerClass.Cellphone;
+                txbName.Text = customerClass.Name;
+                txbBirthdate.Text = customerClass.Birthdate;
+                txbCellphone.Text = customerClass.Cellphone;
                 
-                if(refCustomerClass.GenderID == 1) // genderid 1이면 남자
+                if(customerClass.GenderID == 1) // genderid 1이면 남자
                 {
                     rbtMale.Checked = true;
                     rbtFemale.Checked = false;
                 }
-                else if(refCustomerClass.GenderID == 2) //genderid 2면 여자
+                else if(customerClass.GenderID == 2) //genderid 2면 여자
                 {
                     rbtMale.Checked = false;
                     rbtFemale.Checked = true;
@@ -80,16 +80,16 @@ namespace DKClinic.CustomerProgram
             
         }
         
-        Data.Customer refCustomerClass; 
+        Data.Customer customerClass; 
         
         
         private void btnOK_Click(object sender, EventArgs e)
         {
-            ctmDepChoice ctmDepChoice1 = new ctmDepChoice(); // create ctmDepChoice obj
+            CustomerDepartmentChoiceControl ctmDepChoice = new CustomerDepartmentChoiceControl(); // create ctmDepChoice obj
 
             if (MessageBox.Show("입력하신 내용이 맞습니까?","내용확인",MessageBoxButtons.YesNo) == DialogResult.Yes)//확인 msgbox
             {
-                OnctmDetail(refCustomerClass, ctmDepChoice1); //이벤트 생성
+                OnctmDetail(customerClass, ctmDepChoice); //이벤트 생성
                 //ctmDepChoice1.Show();
                 MessageBox.Show($"ctmDepChoice1.Show()");
                
@@ -117,7 +117,7 @@ namespace DKClinic.CustomerProgram
                 ctmDetail(this, e);
         }
 
-        private ctmDetailEventArgs OnctmDetail(Data.Customer refCustomerClass, ctmDepChoice refCtmDepChoice)
+        private ctmDetailEventArgs OnctmDetail(Data.Customer refCustomerClass, CustomerDepartmentChoiceControl refCtmDepChoice)
         {
             ctmDetailEventArgs args = new ctmDetailEventArgs(refCustomerClass, refCtmDepChoice);
             OnctmDetail(args);
@@ -136,13 +136,13 @@ namespace DKClinic.CustomerProgram
         public class ctmDetailEventArgs : EventArgs
         {
             public Data.Customer RefCustomerClass { get; set; }
-            public ctmDepChoice RefCtmDepChoice { get; set; }
+            public CustomerDepartmentChoiceControl RefCtmDepChoice { get; set; }
 
             public ctmDetailEventArgs()
             {
             }
 
-            public ctmDetailEventArgs(Data.Customer refCustomerClass, ctmDepChoice refCtmDepChoice)
+            public ctmDetailEventArgs(Data.Customer refCustomerClass, CustomerDepartmentChoiceControl refCtmDepChoice)
             {
                 RefCustomerClass = refCustomerClass;
                 RefCtmDepChoice = refCtmDepChoice;
