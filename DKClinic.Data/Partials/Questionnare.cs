aed387c82eba9815a5c7b08ab039933f70157b66
dlehd333 <dlehd333@gmail.com>
2020-11-26 20:44:12 +0900

    Questionnare수정 및 EmployeeManageQuestionnareControl 작업 #54

---
+++ DKClinic.Data/Partials/Questionnare.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DKClinic.Data
+{
+    public partial class Questionnare
+    {
+        public string CustomerName { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
