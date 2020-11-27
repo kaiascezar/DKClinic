77c8c00de9f76346d70b3cb74657e30d1632e8a8
dlehd333 <dlehd333@gmail.com>
2020-11-27 20:59:49 +0900

    employeemanagequestioncontrol수정

--- DKClinic.Data/Dao/QuestionDao.cs
+++ DKClinic.Data/Dao/QuestionDao.cs
@@ -42,13 +42,19 @@ namespace DKClinic.Data
         {
             using (var context = DKClinicEntities.Create())
             {
+                List<Department> departments = Dao.Department.GetAll();
+
                 foreach (Question question in questions)
                 {
-                    var query = context.Questions
-                        .Where(x => x.QuestionID == question.QuestionID)
-                        .Select(x => x.Department.Name)
-                        .FirstOrDefault();
-                    question.DepartmentName = query.ToString();
+                    question.DepartmentName = departments
+                        .Where(x => x.DepartmentID == question.DepartmentID)
+                        .Select(x => x.Name).FirstOrDefault();
+
+                    //var query = context.Questions
+                    //    .Where(x => x.QuestionID == question.QuestionID)
+                    //    .Select(x => x.Department.Name)
+                    //    .FirstOrDefault();
+                    //question.DepartmentName = query.ToString();
     
                     if (question.Type == 1)
                         question.TypeName = "주관식";
@@ -86,13 +92,33 @@ namespace DKClinic.Data
             }
         }
 
+        public List<Question> GetByDepartmentIDAndIndex(List<Question> questions, int departmentID, int index)
+        {
+            using (var context = DKClinicEntities.Create())
+            {
+                var query = questions
+                    .Where(x => x.DepartmentID == departmentID && x.Index == index)
+                    .Select(x => x);
+
+                return query.ToList();
+            }
+        }
+
         public int GetNewestVersionNumber(int departmentId, int index)
         {
             List<Question> questions = GetByDepartmentIDAndIndex(departmentId, index);
+            if (questions.Count == 0) return 0;
             return questions.OrderByDescending(x => x.Version).ToList()[0].Index;
         }
 
-        public List<Question> SelectionNewestVersion(List<Question> questions, int departmentID=0)
+        public int GetNewestVersionNumber(List<Question> questions, int departmentId, int index)
+        {
+            List<Question> questionsList = GetByDepartmentIDAndIndex(questions, departmentId, index);
+            if (questionsList.Count == 0) return 0;
+            return questionsList.OrderByDescending(x => x.Version).ToList()[0].Index;
+        }
+
+        public List<Question> SelectionNewestVersion(List<Question> questions, int departmentID = 0)
         {
             int maxDepartment;
             List<Question> resultList = new List<Question>();
@@ -102,13 +128,27 @@ namespace DKClinic.Data
             else
                 maxDepartment = departmentID;
 
-            for(int i = (departmentID != 0 ? departmentID : 1); i <= maxDepartment; i++)
+            for (int i = (departmentID != 0 ? departmentID : 1); i <= maxDepartment; i++)
             {
-                for(int j = 1; j <= Dao.Department.GetQuestionCount(i); j++)
+                for (int j = 1; j <= Dao.Department.GetQuestionCount(i); j++)
                 {
                     List<Question> list = questions.FindAll(x => x.DepartmentID == i && x.Index == j);
                     resultList.Add(list.OrderByDescending(x => x.Version).ToList()[0]);
                 }
+            }
+
+            return resultList;
+        }
+
+        public List<Question> SelectionNewestVersionByDepartmentID(List<Question> questions, Department department)
+        {
+            int maxDepartment = (int)department.Count;
+            List<Question> resultList = new List<Question>();
+
+            for (int j = 1; j <= department.Count; j++)
+            {
+                List<Question> list = questions.FindAll(x => x.DepartmentID == department.DepartmentID && x.Index == j);
+                resultList.Add(list.OrderByDescending(x => x.Version).ToList()[0]);
             }
 
             return resultList;
